65997f5c83276606d0711334c29d1e33a88d0fa7
Philip Garip <phil.garip@gmail.com>
2021-05-05 09:40:40 -0400

    added prompts

--- Develop/index.js
+++ Develop/index.js
@@ -2,17 +2,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = 
+inquirer
+    .prompt([
     {
-        
+        type: 'input',
+        message: 'What was your motivation for this project?',
+        name: 'motivation'
+    },
+    {
+        type: 'input',
+        message: 'Why did you build this project?',
+        name: 'why'
+    },
+    {
+        type: 'input',
+        message: 'What problem does it solve?',
+        name: 'problem'
+    },
+    {
+        type: 'input',
+        message: 'What did you learn?',
+        name: 'learn'
+    },
+    {
+        type: 'input',
+        message: 'What makes your project stand out?',
+        name: 'standout'
     }
-];
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
