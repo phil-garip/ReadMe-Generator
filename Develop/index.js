79965f78e968ca8cc8faf82f8c43ea2cfe436de6
Philip Garip <phil.garip@gmail.com>
2021-05-05 18:28:19 -0400

    promise handler and test md file for layout

--- Develop/index.js
+++ Develop/index.js
@@ -2,38 +2,49 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = 
+const questions = []
 inquirer
     .prompt([
-    {
-        type: 'input',
-        message: 'What was your motivation for this project?',
-        name: 'motivation'
+        {
+            type: 'input',
+            message: 'What was your motivation for this project?',
+            name: 'motivation'
+        },
+        {
+            type: 'input',
+            message: 'Why did you build this project?',
+            name: 'why'
+        },
+        {
+            type: 'input',
+            message: 'What problem does it solve?',
+            name: 'problem'
+        },
+        {
+            type: 'input',
+            message: 'What did you learn?',
+            name: 'learn'
+        },
+        {
+            type: 'input',
+            message: 'What makes your project stand out?',
+            name: 'standout'
+        }
+    ])
+    .then ((answers) => {
+    const readMe = renderReadMe(answers);
+
+    fs.writeFile("README.md", readMe, "utf8", (err) => {
+        if (err) console.log(err);
+        console.log("We wrote the file.")
+        
+        })
     },
-    {
-        type: 'input',
-        message: 'Why did you build this project?',
-        name: 'why'
-    },
-    {
-        type: 'input',
-        message: 'What problem does it solve?',
-        name: 'problem'
-    },
-    {
-        type: 'input',
-        message: 'What did you learn?',
-        name: 'learn'
-    },
-    {
-        type: 'input',
-        message: 'What makes your project stand out?',
-        name: 'standout'
-    }
-]);
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function renderReadMe ({ motivation, why, problem, learn, standout }) {
+    return
+}
 
 // // TODO: Create a function to initialize app
 // function init() {}
